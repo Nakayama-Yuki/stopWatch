b1db6fdda8976b0a293364c8a0bf58ab405ddd2d
yuki <yuki.for.neutral@gmail.com>
2024-08-12 15:38:11 +0900

    add globalCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // `app` ディレクトリを追加することに注意してください。
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // `src` ディレクトリを使用している場合:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
